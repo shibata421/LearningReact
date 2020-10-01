146ef0c8f5127e236b4425a62b06ffe5ff2715b1
Fernando Shibata <shibata.421@gmail.com>
2020-10-01 14:37:08 -0300

    Rendering lists of objects

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const lakeList = [
-  "Echo Lake",
-  "Maud Lake",
-  "Cascade Lake"
+  {id: 1, name: "Echo", trailhead: "Echo"},
+  {id: 2, name: "Maud", trailhead: "Wrights"},
+  {id: 3, name: "Velma", trailhead: "Bayview"}
 ]
 
 function App({lakes}) {
   return (
-    <ul>
-      {lakes.map(lake => <li>{lake}</li>)}
-    </ul>
+    <div>
+      {lakes.map(lake => 
+        <div>
+          <h2>{lake.name}</h2>
+          <p>Accessed by: {lake.trailhead}</p>
+        </div>
+      )}
+    </div>
   )
 }
 
